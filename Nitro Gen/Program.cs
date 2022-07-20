a87c868809cca69280ea0d4e5e960726cc99ee3d
CHIKI <98632445+CHIKI2022@users.noreply.github.com>
2022-07-20 23:33:23 +0430

    Add files via upload

---
+++ Nitro Gen/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nitro_Gen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Title = "CHIKI GEN - Not Copy, I Think";
+
+            Console.Write("How Enter The Number Of Codes Do You Want : ");
+            int Lines = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPress Any Key To Start !");
+            Console.ReadKey();
+
+            Console.WriteLine("");
+
+            File.WriteAllText("Codes.txt","");
+
+            for(int i = 0; i < (Lines);i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                string nitroCode =  "discord.gift/"+ Discord.REQ.Nitro.GenCode();
+                Console.WriteLine( "[ " + (i + 1) + " ] " +  nitroCode);
+                File.AppendAllText("Codes.txt",(nitroCode + "\n"));
+
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\n Press Any Key To Exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Nitro Gen/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nitro Gen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("17689-22881")]
+[assembly: AssemblyProduct("Nitro Gen")]
+[assembly: AssemblyCopyright("Copyright © 17689-22881 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a6d5521a-7eab-4504-a4f9-ddd4546e056e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
